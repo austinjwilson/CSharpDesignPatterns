00c04f50bb0b4961d8c31efb17f96cb8d70cbde0
Austin Wilson <austinjwilson1989@gmail.com>
2019-10-17 09:49:35 -0500

    Added Base and Abstract Factory

---
+++ CSharpDesignPatterns/CSharpDesignPatterns/Base/AbstractMountainBike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpDesignPatterns.Base
+{
+    public abstract class AbstractMountainBike : AbstractBike
+    {
+        public override decimal Price { get; } = 740.00m;
+
+        public AbstractMountainBike(BikeColor color, IWheel wheel)
+            : base(color, wheel) { }
+
+        public AbstractMountainBike(IWheel wheel)
+            : this(BikeColor.Chrome, wheel) { }
+    }
+}
---
+++ CSharpDesignPatterns/CSharpDesignPatterns/Program.cs
@@ -0,0 +1,25 @@
+﻿using CSharpDesignPatterns.AbstractFactory;
+using System;
+
+namespace CSharpDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            AbstractFactoryDemo();
+        }
+
+        static void AbstractFactoryDemo()
+        {
+            AbstractBikeFactory factory = new RoadBikeFactory();
+            IBikeFrame bikeFrame = factory.CreateBikeFrame();
+            IBikeSeat bikeSeat = factory.CreateBikeSeat();
+
+            Console.WriteLine(bikeFrame.BikeFrameParts);
+            Console.WriteLine(bikeSeat.BikeSeatParts);
+        }
+
+    }
+}
